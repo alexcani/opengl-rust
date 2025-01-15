a7f331856437bef0e0c8d49cedf47e4688af0dc2
Alex Cani <alexcani109@gmail.com>
2025-01-15 21:14:49 +0100

    Create Object and Transform abstractions to handle instance data

--- src/entity.rs
+++ src/entity.rs
@@ -1,3 +1,5 @@
 pub mod camera;
+pub mod object;
 
 pub use camera::Camera;
+pub use object::{Object, Transform};
---
+++ src/entity/object.rs
@@ -0,0 +1,49 @@
+use std::rc::Rc;
+
+use crate::renderer::mesh::Mesh;
+use crate::renderer::shader::ShaderProgram;
+
+pub struct Transform {
+    pub position: glam::Vec3,
+    pub scale: glam::Vec3,
+    pub rotation: glam::Quat,
+}
+
+impl Transform {
+    pub fn new(position: glam::Vec3, scale: glam::Vec3, rotation: glam::Quat) -> Self {
+        Self {
+            position,
+            scale,
+            rotation,
+        }
+    }
+
+    pub fn model_matrix(&self) -> glam::Mat4 {
+        glam::Mat4::from_scale_rotation_translation(self.scale, self.rotation, self.position)
+    }
+}
+
+impl Default for Transform {
+    fn default() -> Self {
+        Self::new(glam::Vec3::ZERO, glam::Vec3::ONE, glam::Quat::IDENTITY)
+    }
+}
+
+pub struct Object {
+    pub transform: Transform,
+    mesh: Rc<Mesh>,
+}
+
+impl Object {
+    pub fn new(mesh: Rc<Mesh>) -> Self {
+        Self {
+            transform: Transform::default(),
+            mesh,
+        }
+    }
+
+    pub fn render(&self, shader: &mut ShaderProgram) {
+        shader.set_uniform_mat4("model", &self.transform.model_matrix());
+        self.mesh.draw();
+    }
+}
