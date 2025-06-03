15413b33916d214d91138345aca74d73d0c1b4bf
Alex Cani <alexcani109@gmail.com>
2025-06-03 20:59:59 +0200

    Implement get/get_mut for ResourceManager

--- src/resource/manager.rs
+++ src/resource/manager.rs
@@ -36,15 +36,6 @@ impl ResourceManager {
         }
     }
 
-    fn storage<T: Resource>(&mut self) -> &mut ResourceStorage<T> {
-        let type_id = TypeId::of::<T>();
-        self.storages
-            .entry(type_id)
-            .or_insert(Box::new(ResourceStorage::<T>::new()))
-            .downcast_mut()
-            .unwrap()
-    }
-
     pub fn load_resource<T: Resource>(&mut self, parameters: &T::Parameters) -> Option<Handle<T>> {
         if let Some(id) = self.find_cached_resource::<T>(parameters) {
             return Some(Handle::new(id));
@@ -54,7 +45,7 @@ impl ResourceManager {
             let id = self.next_id;
             self.next_id += 1;
 
-            let storage = self.storage::<T>();
+            let storage = self.storage_mut::<T>();
             storage.insert_resource(id, resource, parameters.clone());
 
             return Some(Handle::new(id));
@@ -63,9 +54,36 @@ impl ResourceManager {
         None
     }
 
+    pub fn get<T: Resource>(&self, id: ResourceId) -> Option<&T> {
+        let storage = self.storage::<T>()?;
+        storage.resources.get(&id)
+    }
+
+    pub fn get_mut<T: Resource>(&mut self, id: ResourceId) -> Option<&mut T> {
+        let storage = self.storage_mut::<T>();
+        storage.resources.get_mut(&id)
+    }
+
+    /// Get a mutable reference to the resource storage for a specific type
+    /// Creates a new storage if it doesn't exist
+    fn storage_mut<T: Resource>(&mut self) -> &mut ResourceStorage<T> {
+        let type_id = TypeId::of::<T>();
+        self.storages
+            .entry(type_id)
+            .or_insert(Box::new(ResourceStorage::<T>::new()))
+            .downcast_mut()
+            .unwrap()
+    }
+
+    /// Get an immutable reference to the resource storage for a specific type
+    /// Returns None if the storage doesn't exist
+    fn storage<T: Resource>(&self) -> Option<&ResourceStorage<T>> {
+        let type_id = TypeId::of::<T>();
+        self.storages.get(&type_id)?.downcast_ref::<ResourceStorage<T>>()
+    }
+
     fn find_cached_resource<T: Resource>(&self, parameters: &T::Parameters) -> Option<ResourceId> {
-        let storage = self.storages.get(&TypeId::of::<T>())?;
-        let storage = storage.downcast_ref::<ResourceStorage<T>>()?;
+        let storage = self.storage::<T>()?;
         if let Some(&id) = storage.parameters_cache.get(parameters) {
             return Some(id);
         }
